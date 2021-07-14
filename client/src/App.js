a34cb4f1b486ff6da6b9a49fb5badf8030daf2ff
Michael Carchesio <michael.carchesio@gmail.com>
2021-07-14 13:20:08 +0200

    Pulizia codice

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,25 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 function App() {
     return (
         <>
-            <Switch>
-                <Route path='/' exact>
-                    <p>Home</p>
-                </Route>
+            <main>
+                <Switch>
+                    <Route path='/' exact>
+                        <p>Home</p>
+                    </Route>
 
-                <Route path='/preferiti'>
-                    <p>Preferiti</p>
-                </Route>
+                    <Route path='/preferiti'>
+                        <p>Preferiti</p>
+                    </Route>
 
-                <Route path='/accedi'>
-                    <p>Accedi</p>
-                </Route>
+                    <Route path='/accedi'>
+                        <p>Accedi</p>
+                    </Route>
 
-                <Route path='*'>
-                    <Redirect to='/'/>
-                </Route>
-            </Switch>
+                    <Route path='*'>
+                        <Redirect to='/'/>
+                    </Route>
+                </Switch>
+            </main>
             <NavigationBar/>
         </>
     );
