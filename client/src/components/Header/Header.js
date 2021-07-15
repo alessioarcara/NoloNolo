34c49ebc7b479fbf001960c583ddebe517ecfe9b
AleCrimmi <alessiacrimaldi@virgilio.it>
2021-07-15 12:15:59 +0200

    commit di aggiornamento

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import SearchBar from "./SearchBar";
 import classes from "./SearchBar.module.css";
+import backImage from "../../images/background.jpg";
 
 const Header = () => {
     return (
         <>
-            <p className={classes.title}>NoloNolo</p>
-            <SearchBar/>
+            <header>
+                <div className={classes.title}>
+                    <p>NoloNolo Boat</p>
+                </div>
+                <SearchBar/>
+                <div className={classes[`main-image`]}>
+                    <img src={backImage} alt="An image with a boat"/>
+                </div>
+            </header>
         </>
     );
 };
