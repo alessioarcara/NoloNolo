94efff9612a79e510685056fdf50f2abb687f79a
Michael Carchesio <michael.carchesio@gmail.com>
2021-07-16 17:32:30 +0200

    New commit homepage

---
+++ client/src/components/SearchPage/Location.js
@@ -0,0 +1,16 @@
+import classes from './Location.module.css';
+import LocationIcon from "../UI/icons/LocationIcon";
+
+const Location = props => {
+    return (
+        <>
+            <div className={classes.places}>
+                <LocationIcon/>
+                <div>{props.text}</div>
+            </div>
+        </>
+    );
+};
+
+
+export default Location;
