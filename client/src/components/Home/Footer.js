5decf0d679f524b4f3b74004caaebe03146ba92b
Michael Carchesio <michael.carchesio@gmail.com>
2021-07-20 18:50:33 +0200

    Commit update

---
+++ client/src/components/Home/Footer.js
@@ -0,0 +1,17 @@
+import classes from "./Footer.module.css";
+import PencilIcon from "../UI/icons/PencilIcon";
+
+const Footer = () => {
+    return (
+        <div className={classes.foot}>
+            <PencilIcon/>
+            <div className={classes['grid-footer']}>
+                <div>Arcara Alessio</div>
+                <div>Carchesio Michael</div>
+                <div>Crimaldi Alessia</div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
