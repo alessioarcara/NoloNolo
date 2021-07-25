00c191e0453b8d1a11b86bd9278d7f77e560f6a9
alessioarcara <Spammer123@>
2021-07-25 21:29:21 +0200

    DONE: errorBoundary, Backdrop and Portal

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import ReactDOM from "react-dom"
+
 import classes from "./Modal.module.css"
 
-const Modal = ({title, children, fullScreen = true}) => {
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onCancel} />;
+};
+
+const ModalOverlay = ({title, children, onCancel, fullScreen = true}) => {
 
     if (fullScreen) {
         return <div className={classes["modal-fullscreen"]}>{children}</div>
@@ -13,8 +20,34 @@ const Modal = ({title, children, fullScreen = true}) => {
                 <h1>{title}</h1>
             </header>
             <section className={classes["modal-content"]}>{children}</section>
-            <span className={classes["modal-close"]}>&times;</span>
+            <span onClick={onCancel} className={classes["modal-close"]}>&times;</span>
         </div>
+    );
+};
+
+const Modal = (props) => {
+    const [show, setShow] = useState(true)
+
+    const showHandler = () => { setShow(false) }
+
+    if (!show) { return null}
+
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop onCancel={showHandler}/>,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    children={props.children}
+                    fullScreen={props.fullScreen}
+                    onCancel={showHandler}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </>
     )
 }
 
