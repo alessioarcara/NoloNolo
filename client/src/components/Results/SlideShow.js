df9fb72c1ed62f3b963459c981113f7769d4f172
Michael Carchesio <michael.carchesio@gmail.com>
2021-07-29 18:27:23 +0200

    Changes by alessioarcara

--- client/src/components/Results/SlideShow.js
+++ client/src/components/Results/SlideShow.js
@@ -12,47 +12,66 @@ const SlideShow = (props) => {
     const [index, setIndex] = useState(0);
     const images = props.image;
 
+    // const imageRef = useRef(null)
+    // const [isVisible, setIsVisible] = useState(false)
+
+    // const callbackFunction = entries => {
+    //     const [entry] = entries
+    //     setIsVisible(entry.isIntersecting)
+    // }
+
+    // const options = {
+    //     root: null,
+    //     rootMargin: "0px",
+    //     threshold: 0.5
+    // }
+    //
+    // console.log(imageRef)
+    //
+    // useEffect(() => {
+    //     const observer = new IntersectionObserver(callbackFunction, options)
+    //     if (imageRef.current) observer.observe(imageRef.current)
+    //
+    //     return () => { if (imageRef.current) observer.unobserve((imageRef.current)) }
+    // }, [imageRef, options])
+
     const changeClickHandler = () => {
         setIsClicked(state => !state);
     }
-
     const leftClickHandler = () => {
         index === 0
             ? setIndex(images.length - 1)
             : setIndex(index - 1)
     }
-
     const rightClickHandler = () => {
         index === images.length - 1
             ? setIndex(0)
             : setIndex(index + 1)
     }
 
+
     return (
         <>
             <div className={classes.slideshow}>
                 <div className={classes.slideshowSlider} style={{ transform: `translate3d(${-index * 100}%, 0, 0)`}}>
                     <Images
+                        // ref={imageRef}
                         photos={images}
                     />
                 </div>
-
                 <div className={classes.slideshowDots}>
                     <Dots
                         photos={images}
                         onCurrentIndex={index}
                     />
                 </div>
-
                 <div onClick={changeClickHandler}
                      className={isClicked ? `${classes.icon} ${classes.clicked}` : classes.icon}>
                     <HeartIcon className={classes.heart}/>
                 </div>
-
                 <div onClick={leftClickHandler} className={`${classes['style-arrow']} ${classes['show-left']}`}>
                     <LeftArrowIcon/>
                 </div>
-
                 <div onClick={rightClickHandler} className={`${classes['style-arrow']} ${classes['show-right']}`}>
                     <RightArrowIcon/>
                 </div>
