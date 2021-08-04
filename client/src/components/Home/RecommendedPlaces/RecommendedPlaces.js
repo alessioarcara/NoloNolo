24d5050225d2c9fdb5d134a725e513733d32bade
alessioarcara <Spammer123@>
2021-08-04 22:35:44 +0200

    DONE: Refactor RecommendedPlaces.js

--- client/src/components/Home/RecommendedPlaces/RecommendedPlaces.js
+++ client/src/components/Home/RecommendedPlaces/RecommendedPlaces.js
@@ -1,43 +1,19 @@
 import PlaceCard from "./PlaceCard";
 import classes from './RecommendedPlaces.module.css';
 
-const RecommendedPlaces = () => {
-    const DUMMY_LOCATION = [
-        {
-            id: 'l1',
-            title: 'Campania'
-        },
-        {
-            id: 'l2',
-            title: 'Liguria'
-        },
-        {
-            id: 'l3',
-            title: 'Sardegna'
-        },
-        {
-            id: 'l4',
-            title: 'Sicilia'
-        },
-        {
-            id: 'l5',
-            title: 'Toscana'
-        },
-        {
-            id: 'l6',
-            title: 'Puglia'
-        }
-    ]
 
+const places = ['Campania', 'Liguria', 'Sardegna', 'Sicilia', 'Toscana', 'Puglia']
+
+const RecommendedPlaces = () => {
     return (
         <>
             <div className="subtitle">Naviga nelle nostre località</div>
             <div className={classes.list}>
-                {DUMMY_LOCATION.map(place =>
+                {places.map((place, index) => (
                     <PlaceCard
-                        key={place.id}
-                        title={place.title}
-                    />
+                        key={index}
+                        title={place}
+                    />)
                 )}
             </div>
         </>
