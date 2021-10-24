cd58ce306b16fadc7941cd622f865d74c8e23b74
AleCrimmi <alessiacrimaldi@virgilio.it>
2021-10-24 13:02:32 +0200

    started: url manipulation

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -5,15 +5,17 @@ import SelectDates from "./SelectDates/SelectDates";
 
 const Search = ({closeModalHandler}) => {
     const [isNextPage, setNextPage] = useState(false)
+    const [placeIsSent, setPlaceIsSent] = useState('')
 
-    const moveClickHandler = () => {
+    const moveClickHandler = event => {
+        setPlaceIsSent(event.target.innerHTML)
         setNextPage(prevState => !prevState)
     }
 
     return (
         <>
             {!isNextPage && <SelectPlaces closeModalHandler={closeModalHandler} moveClickHandler={moveClickHandler}/>}
-            {isNextPage && <SelectDates moveClickHandler={moveClickHandler}/>}
+            {isNextPage && <SelectDates moveClickHandler={moveClickHandler} sendPlaceHandler={placeIsSent}/>}
         </>
     );
 };
--- client/src/components/Search/SelectDates/SelectDates.js
+++ client/src/components/Search/SelectDates/SelectDates.js
@@ -3,17 +3,27 @@ import SearchActionButtons from "./SearchActionButtons";
 import BackIcon from "../../UI/icons/BackIcon";
 import SearchDatePicker from "./SearchDatePicker";
 import {useCallback} from "react";
+import {useHistory} from "react-router-dom/cjs/react-router-dom";
 
-const SelectDates = ({moveClickHandler}) => {
+
+const SelectDates = ({moveClickHandler, sendPlaceHandler}) => {
+    const history = useHistory()
+    const place = sendPlaceHandler.split(',')[0]
+    const startingDate = ''
+    const endingDate = ''
+
     const cancelSelectionHandler = useCallback(() => {
-
+        
     },[])
+
     const skipClickHandler = useCallback(() => {
+        // ora 'search' si chiama 'results'
+        history.push(`/results?place=${place}`)
+    }, [history, place])
 
-    }, [])
     const goForwardClickHandler = useCallback(() => {
-
-    }, [])
+        history.push(`/results?place=${place}&start=${startingDate}&end=${endingDate}`)
+    }, [history, place, startingDate, endingDate])
 
     return (
         <div className={classes[`datepicker-container`]}>
