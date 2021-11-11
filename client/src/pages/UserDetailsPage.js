ecfd2e337eeb4fd83e637a6bc40900660f84f47e
alessioarcara <Spammer123@>
2021-11-11 14:23:02 +0100

    DONE: improvement for changeForm to useForm, Filter.module.css fix and added two form config

--- client/src/pages/UserDetailsPage.js
+++ client/src/pages/UserDetailsPage.js
@@ -1,6 +1,41 @@
+import useForm from "../hooks/use-form";
+import {addressForm, newPasswordForm} from "../helpers/formConfig";
+import Button from "../components/UI/Button/Button";
+import {useCallback, useState} from "react";
+
+/* PROVVISORIO */
+const userDetails = [
+    {
+        type: "Indirizzo",
+        form: addressForm
+    },
+    {
+        type: "Password",
+        form: newPasswordForm
+    },
+    {
+        type: "Foto"
+    }
+]
+
 const UserDetailsPage = () => {
+    const {renderFormInputs, changeForm} = useForm(addressForm)
+
+    const openDetailsHandler = useCallback(form => {
+        changeForm(form)
+    }, [])
+
     return (
-        <h1>User Details</h1>
+        /* PROVVISORIO */
+        <section className="centered">
+            {userDetails.map((details, idx) => (
+                <div key={idx}>
+                    <p>{details.type}</p>
+                    <Button className="btn btn-secondary" onClick={() => openDetailsHandler(details.form)}>Modifica</Button>
+                    {renderFormInputs()}
+                </div>
+            ))}
+        </section>
     );
 };
 
