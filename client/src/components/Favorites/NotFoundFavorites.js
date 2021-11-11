c96c9b9f0ad30af9df6842d0e8c758121b9b2592
Michael Carchesio <michael.carchesio@gmail.com>
2021-11-11 20:03:58 +0100

    Done: set empty page

--- client/src/components/Favorites/NotFoundFavorites.js
+++ client/src/components/Favorites/NotFoundFavorites.js
@@ -1,11 +1,20 @@
 import classes from './NotFoundFavorites.module.css';
-import NotFoundIcon from "../UI/icons/NotFoundIcon";
+import {Link} from "react-router-dom";
 
 const NotFoundFavorites = () => {
     return (
         <div className={classes['not-found-container']}>
-            {/*<NotFoundIcon/>*/}
-            <p className={classes['not-found-text']}>Non hai aggiunto nessun preferito!</p>
+            <div className={classes['container']}>
+                <div className={classes['not-found-text']}>
+                    Non hai ancora nessuna barca preferita! Premi sul &hearts; per aggiungerle tra i preferiti.
+                </div>
+                <Link
+                    className='btn btn-primary'
+                    to="/"
+                >
+                    Cerca
+                </Link>
+            </div>
         </div>
     );
 }
