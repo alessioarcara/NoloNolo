98655bf8e678881e3a11a3f927d5c948658940de
AleCrimmi <alessiacrimaldi@virgilio.it>
2021-11-13 12:50:20 +0100

    Profile CSS fix

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -4,15 +4,15 @@ import classes from "./Profile.module.css"
 const Profile = ({children, auth}) => {
     return (
         <>
-            <div className='centered'>
-                <h1 className={`title ${classes.title}`}>Profilo</h1>
+            <section className="centered">
+                <h1 className="title">Profilo</h1>
                 <div className={classes.container}>
                     {children}
                 </div>
                 <div className={classes.action}>
                     <Button className={classes.logout} onClick={auth.logout} type="button">Esci</Button>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
