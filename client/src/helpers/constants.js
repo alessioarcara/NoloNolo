a972ac9227a57b622789a3573377ae35acfa5ed1
Michael Carchesio <michael.carchesio@gmail.com>
2021-11-19 18:44:10 +0100

    Done: set constants

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,5 +1,5 @@
-export const GRAPHQL_API_URL = "http://172.20.10.2:3010/api"
-export const IMAGE_PATH = "http://172.20.10.2:3010"
+export const GRAPHQL_API_URL = "http://192.168.1.226:3010/api"
+export const IMAGE_PATH = "http://192.168.1.226:3010"
 export const DEFAULT_AVATAR = `${IMAGE_PATH}/public/images/default.jpg`
 
 /* Constants filterReducer */
