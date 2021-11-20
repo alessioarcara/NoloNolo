5d76edab00f04540ed5e22a3717c04594757a7ba
Michael Carchesio <michael.carchesio@gmail.com>
2021-11-20 09:38:42 +0100

    Done: refactor RentalsFutureCard

--- client/src/components/ShipownerProfile/AnnouncementCard/RentalsFuture/RentalsFutureCard/RentalsFutureCard.js
+++ client/src/components/ShipownerProfile/AnnouncementCard/RentalsFuture/RentalsFutureCard/RentalsFutureCard.js
@@ -4,15 +4,21 @@ import {formatDayMonthYearDate, formatNumber} from "../../../../../helpers/utils
 const RentalsFutureCard = ({customer, createdAt, start, end, totalAmount}) => {
     return (
         <div className={classes['future-container']}>
-            <div>{formatDayMonthYearDate(createdAt, {day: "numeric", month: 'numeric', year: 'numeric'})}</div>
-            <div>{customer.split('@')[0]}</div>
+            <div className={classes['created-at']}>
+                {formatDayMonthYearDate(createdAt, {day: "numeric", month: 'numeric', year: 'numeric'})}
+            </div>
+            <div className={classes['customer']}>
+                {customer.split('@')[0]}
+            </div>
             <span className={classes['date']}>
                 {`
                     ${formatDayMonthYearDate(start, {day: 'numeric', month: 'short', year: 'numeric'})} -
                     ${formatDayMonthYearDate(end, {day: 'numeric', month: 'short', year: 'numeric'})}
                 `}
             </span>
-            <div>{formatNumber(totalAmount)}</div>
+            <div className={classes['total-amount']}>
+                {formatNumber(totalAmount)}
+            </div>
         </div>
     );
 }
