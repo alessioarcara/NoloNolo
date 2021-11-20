6de65256d516a937b46c673436bf5ccdd9c699ab
alessioarcara <Spammer123@>
2021-11-20 12:32:05 +0100

    DONE: commit to move to RentalsPage Branch

---
+++ client/src/hooks/use-geocode.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+const baseUrl = 'https://nominatim.openstreetmap.org/search?'
+
+const UseGeocode = (address) => {
+    const [position, setPosition] = useState()
+
+    const fromAddress = () => {
+        axios.get()
+    }
+    return;
+};
+
+export default UseGeocode;
