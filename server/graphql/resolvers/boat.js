7e0474b0acff5f044a5d02944b0763a81f8f3910
alessioarcara <Spammer123@>
2021-11-23 11:23:46 +0100

    DONE: Fix to updateRental API (fromDate, toDate, transformRental)

--- server/graphql/resolvers/boat.js
+++ server/graphql/resolvers/boat.js
@@ -82,7 +82,6 @@ module.exports = {
             req.userId = "61013cd2cbcb99c21fbe91e2"
             const boats = await Boat.find({"location.region": "emilia-romagna"}).lean()
             console.log("ciao")
-            console.log(boats)
             // return transformBoat(boat)
         } catch (err) { throw new Error(`Can't find boats. ${err}`)}
     },
